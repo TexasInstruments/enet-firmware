12384d835deac246f227fc62e0a061aeedc5cee8
Badri S <badri@ti.com>
2020-02-06 15:18:55 +0530

    app: add webserver to app_remote_client
    added the http server to the remote server
    app
    
    Signed-off-by: Badri S <badri@ti.com>

--- apps/app_remoteswitchcfg_client/mcu_2_1/webpage.c
+++ apps/app_remoteswitchcfg_client/mcu_2_1/webpage.c
@@ -89,6 +89,18 @@
 #include "webdata/titagline.h"
 #include "webdata/sample.h"
 
+typedef void *HANDLE;
+typedef char INT8;
+typedef short INT16;
+typedef int INT32;
+typedef unsigned char UINT8;
+typedef unsigned short UINT16;
+typedef unsigned int UINT32;
+
+typedef UINT32 IPN;
+typedef struct sockaddr *PSA;
+
+
 /* ========================================================================== */
 /*                           Macros & Typedefs                                */
 /* ========================================================================== */
