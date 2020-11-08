TexasInstruments/enet-firmware

commit 033dff7f31f4f965764efb8ef3346776394e1830

[PSDKQA-187][ETHFW-1645] Update PTP Support for Remote Client
- Added PTP init API in EthFW lib for Vision Apps.
- Updated HW Push evnt notify callback to support multiple clients.
- Updated HW Push event notify message structure to avoid padding.

Signed-off-by: Stanley Liu <stanley_liu@ti.com>

ethremotecfg/client/src/cpsw_proxy.c
@@ -939,9 +939,12 @@ static void CpswProxy_notifyServiceTskFxn(UArg a0, UArg a1)
     uint32_t remoteProc, remoteEp;
     CpswRemoteNotifyService_MessageHeader *header = NULL;
     uint16_t len;
-    uint64_t msgBuffer[(CPSW_REMOTE_NOTIFY_SERVICE_RPC_MSG_SIZE / sizeof(uint64_t))];
+    uint8_t msgBuffer[CPSW_REMOTE_NOTIFY_SERVICE_RPC_MSG_SIZE];
     volatile bool exitTask = false;
+    void *data;
+    CpswRemoteNotifyService_HwPushMsg *hwPushMsg;
 
+    data = (void *)msgBuffer;
     /* Create RPMsg */
     RPMessageParams_init(&rpmsgPrm);
     rpmsgPrm.requestedEndpt = CPSW_REMOTE_NOTIFY_SERVICE_ENDPT_ID;
@@ -983,7 +986,7 @@ static void CpswProxy_notifyServiceTskFxn(UArg a0, UArg a1)
     while (!exitTask)
     {
         ret = RPMessage_recv(hProxy->notifyServiceObj.hNotifyServicRpMsgEp,
-                            (Ptr)msgBuffer,
+                            data,
                             &len,
                             &remoteEp,
                             &remoteProc,
@@ -995,17 +998,17 @@ static void CpswProxy_notifyServiceTskFxn(UArg a0, UArg a1)
             CpswProxy_assert(remoteProcId == remoteProc);
 
             /* Process received message */
-            header = (CpswRemoteNotifyService_MessageHeader *)msgBuffer;
+            header = (CpswRemoteNotifyService_MessageHeader *)data;
             switch(header->messageId)
             {
                 case CPSW_REMOTE_NOTIFY_SERVICE_CMD_HWPUSH:
                 {
-                    CpswRemoteNotifyService_HwPushMsg *hwPushMsg = (CpswRemoteNotifyService_HwPushMsg *)msgBuffer;
-
+                    hwPushMsg = (CpswRemoteNotifyService_HwPushMsg *)data;
                     CpswProxy_assert(hwPushMsg->header.messageLen == sizeof(CpswRemoteNotifyService_HwPushMsg));
+
                     if(hProxy->notifyServiceObj.cb.hwPushCb != NULL)
                     {
-                        hProxy->notifyServiceObj.cb.hwPushCb(hwPushMsg->hwPushNum,
+                        hProxy->notifyServiceObj.cb.hwPushCb((CpswCpts_HwPush)hwPushMsg->hwPushNum,
                                                              hwPushMsg->timeStamp);
                     }
 

ethfw/ethfw.h
@@ -298,6 +298,20 @@ int32_t EthFw_lateAnnounce(EthFw_Handle hEthFw,
 void EthFw_getVersion(EthFw_Handle hEthFw,
                       EthFw_Version *version);
 
+/*!
+ * \brief Initialize and Enable EthFw PTP Stack
+ *
+ * Initializes and enable the EthFw PTP Stack with the provided configuration parameters.
+ *
+ * \param ipAddr        IP Address
+ * \param hostMacAddr   Host Port MAC Address
+ * \param portMask      Mask of ports used for PTP. The mask is built by or-ing
+ *                      ENET_BIT(ENET_NORM_MACPORT(macPort)).
+ */
+void EthFw_initTimeSyncPtp(uint32_t ipAddr,
+                           const uint8_t *hostMacAddr,
+                           uint32_t portMask);
+
 /* ========================================================================== */
 /*                        Deprecated Function Declarations                    */
 /* ========================================================================== */

ethfw/src/ethfw.c
@@ -117,6 +117,10 @@
 
 #include <utils/profile/include/app_profile.h>
 
+/* Timesync header files */
+#include <ti/transport/timeSync/v2/include/timeSync.h>
+#include <ti/transport/timeSync/v2/protocol/ptp/include/timeSync_ptp.h>
+
 /* ========================================================================== */
 /*                           Macros & Typedefs                                */
 /* ========================================================================== */
@@ -174,6 +178,10 @@ typedef struct EthFw_Obj_s
 
     /* Multiclient Manager (MCM) handle */
     EnetMcm_CmdIf mcmCmdIf;
+
+    /* Handle to PTP stack */
+    TimeSyncPtp_Handle timeSyncPtp;
+
 } EthFw_Obj;
 
 /* ========================================================================== */
@@ -408,7 +416,8 @@ int32_t EthFw_initRemoteConfig(EthFw_Handle hEthFw)
 
     /* Enable server-to-client notify service */
     cfg.notifyServiceCpswType = gEthFwObj.enetType;
-    cfg.notifyServiceRemoteCoreId = IPC_MCU2_1;
+    cfg.notifyServiceRemoteCoreId[0] = IPC_MPU1_0;
+    cfg.notifyServiceRemoteCoreId[1] = IPC_MCU2_1;
 
     status = CpswProxyServer_init(&cfg);
     if (status != ENET_SOK)
@@ -641,4 +650,41 @@ static void EthFw_handleProfileInfoNotify(uint32_t host_id,
     }
 
     appLogPrintf("***********************************\n");
+}
+
+/* PTP related functions */
+
+static void EthFw_setPtpConfig(TimeSyncPtp_Config *ptpConfig)
+{
+#if defined(SOC_J721E)
+    ptpConfig->socConfig.socVersion = TIMESYNC_SOC_J721E;
+    ptpConfig->socConfig.ipVersion  = TIMESYNC_IP_VER_CPSW_9G;
+#elif defined(SOC_J7200)
+    ptpConfig->socConfig.socVersion = TIMESYNC_SOC_J7200;
+    ptpConfig->socConfig.ipVersion  = TIMESYNC_IP_VER_CPSW_5G;
+#endif
+    ptpConfig->vlanCfg.vlanType     = TIMESYNC_VLAN_TYPE_NONE;
+    ptpConfig->deviceMode           = TIMESYNC_ORDINARY_CLOCK;
+}
+
+void EthFw_initTimeSyncPtp(uint32_t ipAddr,
+                           const uint8_t *hostMacAddr,
+                           uint32_t portMask)
+{
+    TimeSyncPtp_Config ptpConfig;
+
+    /* Initialize and enable PTP stack */
+    TimeSyncPtp_setDefaultPtpConfig(&ptpConfig);
+    EthFw_setPtpConfig(&ptpConfig);
+    ptpConfig.portMask = portMask;
+
+    /* Save host port IP address and MAC address */
+    memcpy(&ptpConfig.ipAddr[0U], &ipAddr, ENET_IPv4_ADDR_LEN);
+    memcpy(&ptpConfig.ifMacID[0U], hostMacAddr, ENET_MAC_ADDR_LEN);
+
+    gEthFwObj.timeSyncPtp = TimeSyncPtp_init(&ptpConfig);
+    EnetAppUtils_assert(gEthFwObj.timeSyncPtp != NULL);
+
+    TimeSyncPtp_enable(gEthFwObj.timeSyncPtp);
+    appLogPrintf("EthFw: TimeSync PTP enabled\n");
 }

ethremotecfg/protocol/cpsw_remote_notify_service.h
@@ -125,7 +125,7 @@ extern "C" {
 #define CPSW_REMOTE_NOTIFY_SERVICE_TASK_PRIORITY        (2U)
 
 /*! Remote notify service task stack size */
-#define CPSW_REMOTE_NOTIFY_SERVICE_TASK_STACKSIZE       (8U * 1024U)
+#define CPSW_REMOTE_NOTIFY_SERVICE_TASK_STACKSIZE       (16U * 1024U)
 
 /* ========================================================================== */
 /*                         Structures and Enums                               */
@@ -149,7 +149,7 @@ typedef enum
 typedef struct CpswRemoteNotifyService_MessageHeader_s
 {
     /*! Message Type enum: CpswRemoteNotifyService_Cmd */
-    uint8_t  messageId;
+    uint32_t messageId;
 
     /*! Message length */
     uint32_t messageLen;
@@ -167,7 +167,7 @@ typedef struct CpswRemoteNotifyService_HwPushMsg_s
     Enet_Type enetType;
 
     /*! CPTS hardware push number */
-    CpswCpts_HwPush hwPushNum;
+    uint32_t hwPushNum;
 
     /*! CPTS hardware push event timestamp  */
     uint64_t timeStamp;

ethremotecfg/server/include/cpsw_proxy_server.h
@@ -195,7 +195,7 @@ typedef struct CpswProxyServer_Config_s
     Enet_Type notifyServiceCpswType;
 
     /*! Remote Core Id for Notification service */
-    uint32_t notifyServiceRemoteCoreId;
+    uint32_t notifyServiceRemoteCoreId[ETHREMOTECFG_SERVER_MAX_INSTANCES];
 
     /*! Remote Core configuration */
     CpswProxyServer_RemoteCoreConfig remoteCoreCfg[ETHREMOTECFG_SERVER_MAX_INSTANCES];

ethremotecfg/server/src/cpsw_proxy_server.c
@@ -168,9 +168,10 @@ typedef struct CpswProxyServer_NotifyServiceObj_s
     EventP_Handle                hHwPushNotifyServiceEvent;
     uint32_t                     hwPushNotifyEventId[CPSW_CPTS_HWPUSH_COUNT_MAX];
     RPMessage_Handle             hNotifyServicRpMsgEp;
-    uint32_t                     dstProc;
+    uint32_t                     dstProc[ETHREMOTECFG_SERVER_MAX_INSTANCES];
     uint32_t                     localEp;
     uint32_t                     remoteEp;
+    uint32_t                     numRemoteCores;
 } CpswProxyServer_NotifyServiceObj;
 
 typedef struct CpswProxyServer_Obj_s
@@ -1494,6 +1495,14 @@ static int32_t CpswProxyServer_registerRemoteTimerHandlerCb(uint32_t host_id,
     Enet_Type enetType;
     CpswProxyServer_Obj *hProxyServer;
 
+    appLogPrintf("Function:%s,HostId:%u,Handle:%p,CoreKey:%x, Name:%s, Timer:%d, PushNum:%u\n",
+                 __func__,
+                 host_id,
+                 hEnet,
+                 core_key,
+                 name,
+                 timer_id,
+                 hwPushNum);
     hProxyServer = CpswProxyServer_getHandle();
     EnetAppUtils_assert((hProxyServer != NULL) && (hProxyServer->initDone == true));
     status = CpswProxyServer_getCpswType(&hProxyServer->handleTbl, hEnet, &enetType);
@@ -1728,7 +1737,11 @@ static int32_t CpswProxyServer_initNotifyServiceEp(CpswProxyServer_Obj * hProxyS
     uint8_t i = 0;
 
     hProxyServer->notifyServiceObj.notifyServiceCpswType = cfg->notifyServiceCpswType;
-    hProxyServer->notifyServiceObj.dstProc = cfg->notifyServiceRemoteCoreId;
+    hProxyServer->notifyServiceObj.numRemoteCores = cfg->numRemoteCores;
+    for (i = 0U; i< cfg->numRemoteCores; i++)
+    {
+        hProxyServer->notifyServiceObj.dstProc[i] = cfg->notifyServiceRemoteCoreId[i];
+    }
     RPMessageParams_init(&comChParam);
     comChParam.numBufs = CPSW_REMOTE_NOTIFY_SERVICE_NUM_RPMSG_BUFS;
     comChParam.buf = g_CpswProxyServerNotifyServiceRpmsgBuf;
@@ -2302,7 +2315,7 @@ static Void CpswProxyServer_notifyServiceTaskFxn(UArg arg0, UArg arg1)
     CpswProxyServer_Obj * hProxyServer = (CpswProxyServer_Obj *)arg0;
     uint64_t msgBuffer[(CPSW_REMOTE_NOTIFY_SERVICE_RPC_MSG_SIZE / sizeof(uint64_t))];
     volatile bool exitTask = false;
-    uint32_t i = 0U, events = 0U;
+    uint32_t i = 0U, events = 0U, j = 0U;
     Enet_IoctlPrms prms;
     CpswCpts_Event lookupEventInArgs;
     CpswCpts_Event lookupEventOutArgs;
@@ -2334,30 +2347,33 @@ static Void CpswProxyServer_notifyServiceTaskFxn(UArg arg0, UArg arg1)
                         hwPushMsg->header.messageId = CPSW_REMOTE_NOTIFY_SERVICE_CMD_HWPUSH;
                         hwPushMsg->header.messageLen = sizeof(*hwPushMsg);
                         hwPushMsg->enetType = hProxyServer->notifyServiceObj.notifyServiceCpswType;
-                        hwPushMsg->hwPushNum = (CpswCpts_HwPush)(i + 1U);
+                        hwPushMsg->hwPushNum = i + 1U;
 
                         lookupEventInArgs.eventType = CPSW_CPTS_EVENTTYPE_HW_TS_PUSH;
-                        lookupEventInArgs.hwPushNum = hwPushMsg->hwPushNum;
+                        lookupEventInArgs.hwPushNum = (CpswCpts_HwPush) hwPushMsg->hwPushNum;
                         lookupEventInArgs.portNum = 0U;
                         lookupEventInArgs.seqId = 0U;
                         lookupEventInArgs.domain  = 0U;
 
-                        ENET_IOCTL_SET_INOUT_ARGS(&prms, &lookupEventInArgs, &lookupEventOutArgs);
-                        rtnVal = Enet_ioctl(hEnet,
-                                            hProxyServer->notifyServiceObj.dstProc,
-                                            CPSW_CPTS_IOCTL_LOOKUP_EVENT,
-                                            &prms);
-                        if (rtnVal == ENET_SOK)
+                        for (j = 0U; j < hProxyServer->notifyServiceObj.numRemoteCores; j++)
                         {
-                            hwPushMsg->timeStamp = lookupEventOutArgs.tsVal;
+                            ENET_IOCTL_SET_INOUT_ARGS(&prms, &lookupEventInArgs, &lookupEventOutArgs);
+                            rtnVal = Enet_ioctl(hEnet,
+                                                hProxyServer->notifyServiceObj.dstProc[j],
+                                                CPSW_CPTS_IOCTL_LOOKUP_EVENT,
+                                                &prms);
+                            if (rtnVal == ENET_SOK)
+                            {
+                                hwPushMsg->timeStamp = lookupEventOutArgs.tsVal;
 
-                            rtnVal = RPMessage_send(hProxyServer->notifyServiceObj.hNotifyServicRpMsgEp,
-                                                    hProxyServer->notifyServiceObj.dstProc,
-                                                    CPSW_REMOTE_NOTIFY_SERVICE_ENDPT_ID,
-                                                    hProxyServer->notifyServiceObj.localEp,
-                                                    hwPushMsg,
-                                                    sizeof(*hwPushMsg));
-                            EnetAppUtils_assert(IPC_SOK == rtnVal);
+                                rtnVal = RPMessage_send(hProxyServer->notifyServiceObj.hNotifyServicRpMsgEp,
+                                                        hProxyServer->notifyServiceObj.dstProc[j],
+                                                        CPSW_REMOTE_NOTIFY_SERVICE_ENDPT_ID,
+                                                        hProxyServer->notifyServiceObj.localEp,
+                                                        hwPushMsg,
+                                                        sizeof(*hwPushMsg));
+                                EnetAppUtils_assert(IPC_SOK == rtnVal);
+                            }
                         }
                     }
                 }

commit fbd7d25be1c5869f71627b33682fe7c9a949cabd

ETHFW-1672 - client: Fix unaligned access notifyService message buffer
A data abort was being observed on the EthFw TI-RTOS client application
when a notification message was received and the register callback was
called.

The abort was reported by BIOS as a "bad calling context" failure
pointing to a semaphore not being called from a Task.  This is not
accurate as the problem was actually caused by an unaligned access in a
"ldm" ARM instruction resulting from accessing hwPushNum and/or
timeStamp members of CpswRemoteNotifyService_HwPushMsg struct.

When issue was observed, stack variable of type
CpswRemoteNotifyService_HwPushMsg was at a 16-bit aligned address, not
32-bit aligned which is required for "ldm" instruction.

This issue was recently introduced by "033dff7 [PSDKQA-187][ETHFW-1645]
Update PTP Support for Remote Client", so the fix consist of reverting
the allocation of IPC message buffer back to a 64-bit array instead of
8-bit array.

Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

ethremotecfg/client/src/cpsw_proxy.c
@@ -939,7 +939,7 @@ static void CpswProxy_notifyServiceTskFxn(UArg a0, UArg a1)
     uint32_t remoteProc, remoteEp;
     CpswRemoteNotifyService_MessageHeader *header = NULL;
     uint16_t len;
-    uint8_t msgBuffer[CPSW_REMOTE_NOTIFY_SERVICE_RPC_MSG_SIZE];
+    uint64_t msgBuffer[CPSW_REMOTE_NOTIFY_SERVICE_RPC_MSG_SIZE / sizeof(uint64_t)];
     volatile bool exitTask = false;
     void *data;
     CpswRemoteNotifyService_HwPushMsg *hwPushMsg;