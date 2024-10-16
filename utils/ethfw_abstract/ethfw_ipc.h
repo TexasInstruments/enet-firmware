0626a75c78d612da470fff68fcec6d77ffe9524c
Meghana Malladi <m-malladi@ti.com>
2024-10-16 15:26:00 +0530

    docs: api_guide: Add API guide documentation support for abstraction layer
    Fixes: ETHFW-2869
    
    Signed-off-by: Meghana Malladi <m-malladi@ti.com>
    Signed-off-by: Sonu Alam <s-alam@ti.com>

--- utils/ethfw_abstract/ethfw_ipc.h
+++ utils/ethfw_abstract/ethfw_ipc.h
@@ -48,10 +48,26 @@
 extern "C" {
 #endif
 
+/*!
+ * \defgroup ETHFW_IPC_ABSTRACT Ethernet Firmware IPC Abstraction APIs
+ *
+ * This section contains Ethernet Firmware IPC APIs. This is a common header
+ * file for both Jacinto and Sitara which holds macros and declarations for IPC
+ * related APIs.
+ * @{
+ */
+/*! @} */
+
+/*!
+ * \addtogroup ETHFW_IPC_ABSTRACT
+ * @{
+ */
+
 /* ========================================================================== */
 /*                                 Macros                                     */
 /* ========================================================================== */
 
+/*! Invalid RPMessage endpoint value */
 #define ETHFW_IPC_INVALID_RPMSG_ENDPOINT           (~1U)
 
 /* ========================================================================== */
@@ -79,32 +95,134 @@ typedef struct EthFwIpc_RpmsgCreateParams_s
 /*                         Global Variables Declarations                      */
 /* ========================================================================== */
 
+/*!
+ * \brief ETHFW IPC RPMessage Handle
+ */
 typedef void *EthFwIpc_RpmsgHandle;
 
 /* ========================================================================== */
 /*                          Function Declarations                             */
 /* ========================================================================== */
 
+/*!
+ * \brief ETHFW IPC initialization
+ *
+ * For Jacinto based caller, this API initialize IPC modile. This is the very first
+ * API to be invoked to initialize internal data structure and utilities required.
+ * For Sitara, it sets global pool for RPMessage Objects to zero. It also initializes
+ * the local remotecore table, which holds details of the remote cores which will 
+ * participate in IPC communication with ETHFW.
+ *
+ * \param selfId remotecore ID of ETHFW
+ * \param numProc number of cores participating in IPC communication with ETHFW
+ * \param procArray Array of cores participating in IPC communication with ETHFW
+ * \param func For Jacinto this function pointer is used to print debug/info message, if not NULL.
+ *             For Sitara this would be a don't care as this is not supported for MCU+SDK.
+ * 
+ * \return Status of the function
+ * 
+ */
 int32_t EthFwIpc_init(uint32_t selfId,
                       uint16_t numProc,
                       uint32_t procArray[IPC_MAX_PROCS],
                       void (*func)(const char *str));
 
+/*!
+ * \brief ETHFW IPC VirtIO initialization
+ *
+ * For Jacinto based caller, this API initializes VRings for Tx and Rx for all the participating
+ * remote cores in IPC communication. Incase of Sitara, this is an empty function as this is already
+ * handled by sysconfig auto-generated code.
+ *
+ * \param numProc number of cores participating in IPC communication with ETHFW
+ * \param vqObj base address of the vq object
+ * \param vringAddr base address of the shared vring of all the cores
+ * \param vringBufSize buffer size of the shared vring 
+ * 
+ * \return Status of the function
+ * 
+ */
 int32_t EthFwIpc_initVirtIO(uint16_t numProc,
                             void *vqObj,
                             void *vringAddr,
                             uint32_t vringBufSize);
 
+/*!
+ * \brief ETHFW IPC RPMessage Module initialization
+ *
+ * For Jacinto based caller, this API initializes RPMessage module and must be called before calling
+ * any other RPMessage functions. Incase of Sitara, this function registers for control endpoint callback
+ * which is necessary for receiving announce endpoints.
+ *
+ * \param rpmsgBuff Buffer pointer to store RPMessage Object
+ * \param taskBuff Buffer used by stack for control task
+ * \param selfCoreId remotecore Id of ETHFW
+ * 
+ * \return Status of the function
+ * 
+ */
 int32_t EthFwIpc_initRpmsg(void *rpmsgBuff, void *taskBuff, uint32_t selfCoreId);
 
+/*!
+ * \brief ETHFW IPC Remote Ready
+ *
+ * API to check if remote core (Linux) has booted and is ready
+ * 
+ * \param coreId remotecoreId of the remotecore user wants to check if ready. This arguments is currently
+ *               a don't care because MCU+SDK doesn't support for any other core other than Linux.
+ * \param timeout In case of Sitara user can specify timeout for this check function, but for Jacinto this 
+ *                API waits forever till Linux is ready.
+ * 
+ * \return Status of the function
+ */
 int32_t EthFwIpc_isRemoteReady(uint32_t coreId, uint32_t timeout);
 
+/*!
+ * \brief ETHFW IPC Late initialization for a remote core
+ *
+ * IPC late init for a remote core. This API is used once Linux is ready.
+ * 
+ * \param coreId remotecoreId of the remotecore user wants to check if ready. This arguments is currently
+ *               a don't care because MCU+SDK doesn't support for any other core other than Linux.
+ * 
+ * \return Status of the function
+ */
 int32_t EthFwIpc_lateInit(uint32_t coreId);
 
+/*!
+ * \brief ETHFW IPC RPMessage params initialization
+ *
+ * Initialize an RPMessage_Params structure to default values.
+ * 
+ * \param params Address of the \ref EthFwIpc_RpmsgCreateParams structure to be initialized.
+ */
 void EthFwIpc_initRpmsgParams(EthFwIpc_RpmsgCreateParams *params);
 
+/*!
+ * \brief ETHFW IPC RPMessage create endpoint instance.
+ *
+ * Create RPMessage endpoint instance.
+ * 
+ * \param params Address of the \ref EthFwIpc_RpmsgCreateParams after updating.
+ * 
+ * \return \ref EthFwIpc_RpmsgHandle on success or a NULL if could not allocate object
+ */
 EthFwIpc_RpmsgHandle EthFwIpc_createRpmsg(EthFwIpc_RpmsgCreateParams *params);
 
+/*!
+ * \brief ETHFW IPC RPMessage send message
+ *
+ * Sends data to a remote processor.
+ * 
+ * \param handle \ref EthFwIpc_RpmsgHandle created during \ref EthFwIpc_createRpmsg for that endpoint.
+ * \param dstProcId Destination ProcId.
+ * \param dstEndPt  Destination Endpoint.
+ * \param srcEndPt  Source Endpoint.
+ * \param data      Data payload to be copied and sent.
+ * \param len       Amount of data to be copied.
+ * 
+ * \return Status of the function
+ */
 int32_t EthFwIpc_sendRpmsg(EthFwIpc_RpmsgHandle handle,
                            uint32_t dstProcId,
                            uint32_t dstEndPt,
@@ -112,6 +230,20 @@ int32_t EthFwIpc_sendRpmsg(EthFwIpc_RpmsgHandle handle,
                            void *data,
                            uint32_t len);
 
+/*!
+ * \brief ETHFW IPC RPMessage receive message
+ *
+ * Receives a message from an endpoint instance
+ * 
+ * \param handle \ref EthFwIpc_RpmsgHandle created during \ref EthFwIpc_createRpmsg for that endpoint.
+ * \param data       Pointer to the client's data buffer.
+ * \param len        Amount of data received.
+ * \param remoteEndPt  Endpoint of source (for replies).
+ * \param remoteProcId ProcId of source (for replies).
+ * \param timeout    Maximum duration to wait for a message in microseconds.
+ * 
+ * \return Status of the function
+ */
 int32_t EthFwIpc_recvRpmsg(EthFwIpc_RpmsgHandle handle,
                            void *data,
                            uint16_t *len,
@@ -119,26 +251,81 @@ int32_t EthFwIpc_recvRpmsg(EthFwIpc_RpmsgHandle handle,
                            uint32_t *remoteProcId,
                            uint32_t timeout);
 
+/*!
+ * \brief ETHFW IPC RPMessage get remote endpoint
+ *
+ * Wait for an endpoint to become available on another processor.
+ * 
+ *  \param currProcId    Remote processor ID
+ *  \param name      Name of the endpoint
+ *  \param remoteProcId    Remote processor ID
+ *  \param remoteEndPt     Remote endpoint ID
+ *  \param timeout   Timeout value (in system ticks)
+ * 
+ * \return Status of the function
+ */
 int32_t EthFwIpc_getRemoteEndPt(uint32_t currProcId,
                                 const char* name,
                                 uint32_t *remoteProcId,
                                 uint32_t *remoteEndPt,
                                 uint32_t timeout);
 
+/*!
+ * \brief ETHFW IPC RPMessage announce to a all cores
+ *
+ * Announce local endpoint to all cores participating in the IPC communication
+ * 
+ *  \param localEp local endpoint which needs to be announced
+ *  \param name    Service name associated with the local endpoint
+ * 
+ * \return Status of the function
+ */
 int32_t EthFwIpc_announceAll(uint32_t localEp,
                              const char* name);
 
+/*!
+ * \brief ETHFW IPC RPMessage announce to a given remotecore
+ *
+ * Announce local endpoint to a given remote core
+ * 
+ *  \param remoteProcId    Remote processor ID
+ *  \param localEp         local endpoint which needs to be announced
+ *  \param name    Service name associated with the local endpoint
+ * 
+ * \return Status of the function
+ */
 int32_t EthFwIpc_announce(uint32_t remoteProcId,
                           uint32_t localEp,
                           const char* name);
 
+
+/*!
+ * \brief ETHFW IPC RPMessage unblock
+ *
+ * Unblocks an RPMessage_recv()
+ * 
+ * \param handle \ref EthFwIpc_RpmsgHandle created during \ref EthFwIpc_createRpmsg for that endpoint.
+ */
 void EthFwIpc_unblockRpmsg(EthFwIpc_RpmsgHandle handle);
 
+/*!
+ * \brief ETHFW IPC RPMessage delete endpoint
+ *
+ *  This function deletes a created endpoint instance. If the
+ *  message queue is non-empty, any messages remaining in the queue
+ *  will be lost.
+ * 
+\param handle \ref EthFwIpc_RpmsgHandle created during \ref EthFwIpc_createRpmsg for that endpoint.
+ * 
+ * \return Status of the function
+ */
 int32_t EthFwIpc_deleteRpmsg(EthFwIpc_RpmsgHandle handle);
 
 
 #ifdef __cplusplus
 }
 #endif
+
+/*! @} */
 
 #endif /* ETHFW_IPC_H_ */
--- utils/ethfw_abstract/ethfw_osal.h
+++ utils/ethfw_abstract/ethfw_osal.h
@@ -48,22 +48,37 @@
 extern "C" {
 #endif
 
+/*!
+ * \defgroup ETHFW_OSAL_ABSTRACT Ethernet Firmware OSAL Abstraction APIs
+ *
+ * This section contains Ethernet Firmware OSAL APIs. This is a common header
+ * file for both Jacinto and Sitara which holds macros and declarations for OSAL
+ * related APIs.
+ * @{
+ */
+/*! @} */
+
+/*!
+ * \addtogroup ETHFW_OSAL_ABSTRACT
+ * @{
+ */
+
 /* ========================================================================== */
 /*                                 Macros                                     */
 /* ========================================================================== */
 
-/** Timer will be started by the user */
+/*! Timer will be started by the user */
 #define ETHFWCLOCK_STARTMODE_USER               ((uint8_t) 0U)
-/** Timer starts automatically after create or scheduler start */
+/*! Timer starts automatically after create or scheduler start */
 #define ETHFWCLOCK_STARTMODE_AUTO               ((uint8_t) 1U)
-/** Timer runs for a single period values and stops */
+/*! Timer runs for a single period values and stops */
 #define ETHFWCLOCK_RUNMODE_ONESHOT              ((uint8_t) 0U)
-/** Timer is periodic and runs continuously */
+/*! Timer is periodic and runs continuously */
 #define ETHFWCLOCK_RUNMODE_CONTINUOUS           ((uint8_t) 1U)
 
-/* EventP_wait will return when ANY of the bits set in mask are set in the Event bits */
+/*! EventP_wait will return when ANY of the bits set in mask are set in the Event bits */
 #define ETHFWEVENT_WAITMODE_ANY                 ((uint8_t) 0U)
-/* EventP_wait will return when ALL the bits set in mask are set in the Event bits */
+/*! EventP_wait will return when ALL the bits set in mask are set in the Event bits */
 #define ETHFWEVENT_WAITMODE_ALL                 ((uint8_t) 1U)
 
 /* ========================================================================== */
@@ -73,7 +88,7 @@ extern "C" {
 /*!
  * \brief Task configuration params
  *
- * common task config params for Jacinto and Sitara
+ * Common task config params for Jacinto and Sitara
  */
 typedef struct EthFwOsal_TaskParams_s
 {
@@ -128,113 +143,410 @@ typedef MailboxP_Params EthFwOsal_MailboxParams;
 /*                         Global Variables Declarations                      */
 /* ========================================================================== */
 
+/*!
+ * \brief ETHFW OSAL Task Handle
+ */
 typedef void *EthFwOsal_TaskHandle;
 
+/*!
+ * \brief ETHFW OSAL Semaphore Handle
+ */
 typedef void *EthFwOsal_SemHandle;
 
+/*!
+ * \brief ETHFW OSAL Clock Handle
+ */
 typedef void *EthFwOsal_ClockHandle;
 
+/*!
+ * \brief ETHFW OSAL Event Handle
+ */
 typedef void *EthFwOsal_EventHandle;
 
+/*!
+ * \brief ETHFW OSAL Mutex Handle
+ */
 typedef void *EthFwOsal_MutexHandle;
-
+/*!
+ * \brief ETHFW OSAL Mailbox Handle
+ */
 typedef void *EthFwOsal_MailboxHandle;
 
 /* ========================================================================== */
 /*                          Function Declarations                             */
 /* ========================================================================== */
 
+/*!
+ * \brief ETHFW OSAL init
+ * 
+ * Based on the caller of the function (Jacinto or Sitara), this function initiates
+ * the OSAL module in ETHFW. For Jacinto, this is a empty function and does nothing,
+ * but for Sitara, this inits and sets the global OSAL pools to zero, which is needed
+ * to support static memory allocation of the OS modules.
+ */
 void EthFwOsal_init(void);
 
 /* -----------------------TaskP APIs---------------------------------------*/
 
+/*!
+ * \brief ETHFW OSAL Task init configuration parameters.
+ *
+ * Sets the Task init configuration parameters. This function sets to default init
+ * params for Task init.
+ *
+ * \param params Init configuration params.
+ * 
+ */
 void EthFwOsal_initTaskParams(EthFwOsal_TaskParams *params);
 
+/*!
+ * \brief ETHFW OSAL function to create a task.
+ *
+ * \param func Function pointer of the task.
+ * 
+ * \param params Pointer to the instance configuration parameters.
+ * 
+ * \return \ref EthFwOsal_TaskHandle on success or a NULL on an error
+ * 
+ */
 EthFwOsal_TaskHandle EthFwOsal_createTask(void (*func)(void*), EthFwOsal_TaskParams *params);
 
+/*!
+ * \brief ETHFW OSAL function to delete a task.
+ *
+ * \param taskHandle \ref EthFwOsal_TaskHandle returned from \ref EthFwOsal_createTask
+ * 
+ * \return Status of the function
+ * 
+ */
 int32_t EthFwOsal_deleteTask(EthFwOsal_TaskHandle *taskHandle);
 
+/*!
+ * \brief ETHFW OSAL function to exit from a task.
+ *
+ * This function does nothing when the caller is from Jacinto baseline, but the same
+ * exits at the end of the task for Sitara.
+ */
 void EthFwOsal_exitTask(void);
 
+/*!
+ * \brief ETHFW OSAL function to check if a given task is terminated
+ *
+ * This function is not supported for Sitara baseline. For a Jacinto caller this function
+ * returns the current status of a task if terminated.
+ * 
+ * \param taskHandle \ref EthFwOsal_TaskHandle returned from \ref EthFwOsal_createTask
+ * 
+ * \return 0 if task is not terminated 1 if task is terminated
+ */
 uint32_t EthFwOsal_isTaskTerminated(EthFwOsal_TaskHandle taskHandle);
 
+/*!
+ * \brief ETHFW OSAL function returns the Task handle of current task
+ *
+ * This function is not supported for Sitara baseline. For a Jacinto caller this function
+ * returns the Task handle of current task.
+ * 
+ * \return \ref EthFwOsal_TaskHandle of current task
+ */
 EthFwOsal_TaskHandle EthFwOsal_getTaskSelf(void);
 
+/*!
+ * \brief ETHFW OSAL function to update Task priority
+ *
+ * This function is not supported for Sitara baseline. For a Jacinto caller this function
+ * to update Task priority.
+ * 
+ * \param taskHandle \ref EthFwOsal_TaskHandle of task
+ * \param priority New priority to be set
+ */
 void EthFwOsal_setTaskPrio(EthFwOsal_TaskHandle taskHandle, uint32_t priority);
 
+/*!
+ * \brief ETHFW OSAL function Task sleep in units of msecs
+ */
 void EthFwOsal_sleepTaskinMsecs(uint32_t timeoutInMsecs);
 
+/*!
+ * \brief ETHFW OSAL function to get time in micro seconds
+ */
 uint64_t EthFwOsal_getTimeInUsecs(void);
 
+/*!
+ * \brief ETHFW OSAL function Task sleep in units of OS tick
+ */
 void EthFwOsal_sleepTask(uint32_t timeout);
 
+/*!
+ * \brief ETHFW OSAL function to Yield processor to equal priority task
+ */
 void EthFwOsal_yieldTask(void);
 
 /* -----------------------CacheP APIs---------------------------------------*/
 
+/*!
+ * \brief ETHFW OSAL function to write back cache lines
+ * 
+ * \param addr Start address of the cache line/s
+ * \param size size (in bytes) of the memory to be written back
+ */
 void EthFwOsal_wbCache(const void *addr, uint32_t size);
 
+/*!
+ * \brief ETHFW OSAL function to invalidate cache lines
+ * 
+ * \param addr Start address of the cache line/s
+ * \param size size (in bytes) of the memory to be written back
+ */
 void EthFwOsal_invCache(const void *addr, uint32_t size);
 
+/*!
+ * \brief ETHFW OSAL function to write back and invalidate cache lines
+ * 
+ * \param addr Start address of the cache line/s
+ * \param size size (in bytes) of the memory to be written back
+ */
 void EthFwOsal_wbInvCache(const void *addr, uint32_t size);
 
 /* -----------------------SemaphoreP APIs------------------------------------*/
 
+/*!
+ * \brief ETHFW OSAL function to create a semaphore.
+ *
+ * \param count Initial count of the semaphore. For binary semaphores,
+ *              only values of 0 or 1 are valid.
+ * 
+ * \return \ref EthFwOsal_SemHandle on success or a NULL on an error
+ * 
+ */
 EthFwOsal_SemHandle EthFwOsal_createSemaphore(uint32_t count);
 
+/*!
+ * \brief ETHFW OSAL function to delete a semaphore.
+ *
+ * \param handle \ref EthFwOsal_SemHandle returned from \ref EthFwOsal_createSemaphore
+ * 
+ * \return Status of the function
+ */
 int32_t EthFwOsal_deleteSemaphore(EthFwOsal_SemHandle handle);
 
+/*!
+ * \brief ETHFW OSAL function to pend a semaphore.
+ *
+ * \param handle \ref EthFwOsal_SemHandle returned from \ref EthFwOsal_createSemaphore
+ * \param timeout Timeout (in ticks) to wait for the semaphore to be signalled (posted)
+ * 
+ * \return Status of the function
+ * 
+ */
 int32_t EthFwOsal_pendSemaphore(EthFwOsal_SemHandle handle, uint32_t timeout);
 
+/*!
+ * \brief ETHFW OSAL function to post a semaphore.
+ *
+ * \param handle \ref EthFwOsal_SemHandle returned from \ref EthFwOsal_createSemaphore
+ * 
+ * \return Status of the function
+ */
 int32_t EthFwOsal_postSemaphore(EthFwOsal_SemHandle handle);
 
 /* -----------------------MutexP APIs------------------------------------*/
 
+/*!
+ * \brief ETHFW OSAL function to create a mutex.
+ *
+ * \return \ref EthFwOsal_MutexHandle on success or a NULL on an error
+ * 
+ */
 EthFwOsal_MutexHandle EthFwOsal_createMutex(void);
 
+/*!
+ * \brief ETHFW OSAL function to post a mutex.
+ *
+ * \param EthFwOsal_MutexHandle \ref EthFwOsal_MutexHandle returned from \ref EthFwOsal_createMutex
+ * 
+ */
 void EthFwOsal_deleteMutex(void *EthFwOsal_MutexHandle);
 
+/*!
+ * \brief ETHFW OSAL function to lock a muex.
+ *
+ * \param EthFwOsal_MutexHandle \ref EthFwOsal_MutexHandle returned from \ref EthFwOsal_createMutex
+ * 
+ */
 void EthFwOsal_lockMutex(void *EthFwOsal_MutexHandle);
 
+/*!
+ * \brief ETHFW OSAL function to unlock a mutex.
+ *
+ * \param EthFwOsal_MutexHandle \ref EthFwOsal_MutexHandle returned from \ref EthFwOsal_createMutex
+ * 
+ */
 void EthFwOsal_unlockMutex(void *EthFwOsal_MutexHandle);
 
 /* -----------------------ClockP APIs------------------------------------*/
 
+/*!
+ * \brief ETHFW OSAL Clock init configuration parameters.
+ *
+ * Sets the Clock init configuration parameters. This function sets to default init
+ * params for Clock init.
+ *
+ * \param params Init configuration params.
+ * 
+ */
 void EthFwOsal_initClockParams(EthFwOsal_ClockParams *params);
 
+/*!
+ * \brief ETHFW OSAL function to create a clock.
+ *
+ * \param func Function pointer of the clock function.
+ * 
+ * \param params Pointer to the instance configuration parameters.
+ * 
+ * \return \ref EthFwOsal_ClockHandle on success or a NULL on an error
+ * 
+ */
 EthFwOsal_ClockHandle EthFwOsal_createClock(void (*func)(void*), EthFwOsal_ClockParams *params);
 
+/*!
+ * \brief ETHFW OSAL function to stack a clock.
+ *
+ * \param handle \ref EthFwOsal_ClockHandle returned from \ref EthFwOsal_createClock
+ * 
+ * \return Status of the function
+ * 
+ */
 int32_t EthFwOsal_startClock(EthFwOsal_ClockHandle handle);
 
+/*!
+ * \brief ETHFW OSAL function to stop a clock.
+ *
+ * \param handle \ref EthFwOsal_ClockHandle returned from \ref EthFwOsal_createClock
+ * 
+ * \return Status of the function
+ * 
+ */
 int32_t EthFwOsal_stopClock(EthFwOsal_ClockHandle handle);
 
+/*!
+ * \brief ETHFW OSAL function to delete a clock.
+ *
+ * \param handle \ref EthFwOsal_ClockHandle returned from \ref EthFwOsal_createClock
+ * 
+ * \return Status of the function
+ * 
+ */
 int32_t EthFwOsal_deleteClock(EthFwOsal_ClockHandle handle);
 
 /* -----------------------EventP APIs------------------------------------*/
 
+/*!
+ * \brief ETHFW OSAL Event init configuration parameters.
+ *
+ * Sets the Event init configuration parameters. This function sets to default init
+ * params for Event init.
+ *
+ * \param params Init configuration params.
+ * 
+ */
 void EthFwOsal_initEventParams(EthFwOsal_EventParams *params);
 
+/*!
+ * \brief ETHFW OSAL function to create a event.
+ *
+ * \param params Pointer to the instance configuration parameters.
+ * 
+ * \return \ref EthFwOsal_EventHandle on success or a NULL on an error
+ * 
+ */
 EthFwOsal_EventHandle EthFwOsal_createEvent(EthFwOsal_EventParams *params);
 
+/*!
+ * \brief ETHFW OSAL function to wait for an event.
+ *
+ * \param handle \ref EthFwOsal_EventHandle returned during \ref EthFwOsal_createEvent
+ * \param eventMask mask of eventIds to pend on (must be non-zero)
+ *                  Only supports upto 24 bits.
+ * \param waitMode Event wait mode.
+ * \param timeout return from wait() after this many system time units
+ * 
+ * \return All consumed events or zero if timeout
+ * 
+ */
 uint32_t EthFwOsal_waitEvent(EthFwOsal_EventHandle handle,
                             uint32_t eventMask,
                             uint8_t waitMode,
                             uint32_t timeout);
 
+/*!
+ * \brief ETHFW OSAL function to post an event.
+ *
+ * \param handle \ref EthFwOsal_EventHandle returned during \ref EthFwOsal_createEvent
+ * \param eventBits mask of eventIds to post (must be non-zero)
+ *
+ * \return Status of the function
+ * 
+ */
 int32_t EthFwOsal_postEvent(EthFwOsal_EventHandle handle, uint32_t eventBits);
 
 /* ----------------------MailboxP APIs------------------------------------*/
 
+/*!
+ * \brief ETHFW OSAL Mailbox init configuration parameters.
+ *
+ * Sets the Mailbox init configuration parameters. This function sets to default init
+ * params for Mailbox init.
+ *
+ * \param params Init configuration params.
+ * 
+ */
 void EthFwOsal_initMailboxParams(EthFwOsal_MailboxParams *params);
 
+/*!
+ * \brief ETHFW OSAL function to create a Mailbox.
+ *
+ * \param params Pointer to the instance configuration parameters.
+ * 
+ * \return \ref EthFwOsal_MailboxHandle on success or a NULL on an error
+ * 
+ */
 EthFwOsal_MailboxHandle EthFwOsal_createMailbox(EthFwOsal_MailboxParams *params);
 
+/*!
+ * \brief ETHFW OSAL function to delete a Mailbox.
+ *
+ * \param handle \ref EthFwOsal_MailboxHandle returned from \ref EthFwOsal_createMailbox
+ * 
+ * \return Status of the function
+ * 
+ */
 int32_t EthFwOsal_deleteMailbox(EthFwOsal_MailboxHandle handle);
 
+/*!
+ * \brief ETHFW OSAL function to post a Mailbox.
+ *
+ * \param handle \ref EthFwOsal_MailboxHandle returned from \ref EthFwOsal_createMailbox
+ * \param msg Pointer to the message to post
+ * \param timeout Timeout (in milliseconds) to wait for post a
+ *                message to the mailbox.
+ * 
+ * \return Status of the function
+ */
 int32_t EthFwOsal_postMailbox(EthFwOsal_MailboxHandle handle,
                               void *msg,
                               uint32_t timeout);
 
+/*!
+ * \brief ETHFW OSAL function to pend a Mailbox.
+ *
+ * \param handle \ref EthFwOsal_MailboxHandle returned from \ref EthFwOsal_createMailbox
+ * \param msg Pointer to the message to pend
+ * \param timeout Timeout (in milliseconds) to wait for pend a
+ *                message to the mailbox.
+ * 
+ * \return Status of the function
+ */
 int32_t EthFwOsal_pendMailbox(EthFwOsal_MailboxHandle handle,
                               void *msg,
                               uint32_t timeout);
@@ -243,5 +555,7 @@ int32_t EthFwOsal_pendMailbox(EthFwOsal_MailboxHandle handle,
 #ifdef __cplusplus
 }
 #endif
+
+/*! @} */
 
 #endif /* ETHFW_OSAL_H_ */
