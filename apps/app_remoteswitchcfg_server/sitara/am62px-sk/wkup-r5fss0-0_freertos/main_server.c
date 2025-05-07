8b59a8c1b2b703271a8537127acd1a523bccad97
thilak <t-karanam@ti.com>
2025-05-07 10:01:52 +0530

    am62px: enet: Update Bootloader thread stack size
    - Update the bootloader thread stack size
      (genbuild change)
    
    Fixes: SITSW-6980
    
    Signed-off-by: Thilak <t-karanam@ti.com>

--- apps/app_remoteswitchcfg_server/sitara/am62px-sk/wkup-r5fss0-0_freertos/main_server.c
+++ apps/app_remoteswitchcfg_server/sitara/am62px-sk/wkup-r5fss0-0_freertos/main_server.c
@@ -46,13 +46,14 @@
 
 
 #define TASK_SIZE (16384U/sizeof(configSTACK_DEPTH_TYPE))
+#define TASK_BOOTLOADER_SIZE    (8192U)
 
 StackType_t gMainTaskStack[TASK_SIZE] __attribute__((aligned(32)));
 StaticTask_t gMainTaskObj;
 TaskHandle_t gMainTask;
 DM_LPMData_t gDMLPMData __attribute__((section(".lpm_data"), aligned(4)));
 
-StackType_t gBootTaskStack[TASK_SIZE] __attribute__((aligned(32)));
+StackType_t gBootTaskStack[TASK_BOOTLOADER_SIZE] __attribute__((aligned(32)));
 StaticTask_t gBootTaskObj;
 TaskHandle_t gBootTask;
 
